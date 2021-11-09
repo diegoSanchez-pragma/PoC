c1e18b675af6e7e3828249942e515f7690bc310b
Diego Sanchez <diego.sanchez@pragma.com.co>
2021-11-08 21:12:06 -0500

    refactor code to mantain the dry pattern

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,7 @@ import os
 def main():
     input_file = 'pipeline_data/input/el_quijote.txt'
     output_file = 'pipeline_data/output/'
-    run_pipeline_2(input_file)
+    run_pipeline_counter(input_file)
     files = os.listdir('.')
     filename = ''
     for file in files:
@@ -21,7 +21,6 @@ def healthy_text(word):
     to_remove = [',', '.', '-', ':', ' ', "'", '"']
     for symbol in to_remove:
         word = word.replace(symbol,'')
-
     word = word.lower()
     word = word.replace('á','a')
     word = word.replace('é','e')
@@ -32,31 +31,28 @@ def healthy_text(word):
 
 def run_pipeline_asc(input_file, output_file, shape):
     with beam.Pipeline(options = PipelineOptions()) as p:
-        words = read_data_and_split(p,input_file) 
-        clean_words = words | beam.Map(healthy_text)
-        count_words = clean_words | beam.combiners.Count.PerElement()
-        ascending_sort = count_words | beam.combiners.Top.Of(shape,key = lambda x: x[1])
+        words = read_data_and_prepare(p,input_file) 
+        ascending_sort = words | beam.combiners.Top.Of(shape,key = lambda x: x[1])
         ascending_sort | beam.io.WriteToText(f'{output_file}ascend', file_name_suffix='.txt')
 
 def run_pipeline_desc(input_file, output_file, shape):
     with beam.Pipeline(options = PipelineOptions()) as p:
-        words = read_data_and_split(p,input_file) 
-        clean_words = words | beam.Map(healthy_text)
-        count_words = clean_words | beam.combiners.Count.PerElement()
-        descending_sort = count_words | beam.combiners.Top.Of(shape,key = lambda x: x[1], reverse = True)
+        words = read_data_and_prepare(p,input_file) 
+        descending_sort = words | beam.combiners.Top.Of(shape,key = lambda x: x[1], reverse = True)
         descending_sort | beam.io.WriteToText(f'{output_file}descend', file_name_suffix='.txt')
 
-def run_pipeline_2(input_file):
+def run_pipeline_counter(input_file):
     with beam.Pipeline(options=PipelineOptions()) as pipeline2:
-        words =  read_data_and_split(pipeline2,input_file)
-        count_words = words | beam.combiners.Count.PerElement()
-        total_tuples = count_words | beam.combiners.Count.Globally() 
+        words =  read_data_and_prepare(pipeline2,input_file)
+        total_tuples = words | beam.combiners.Count.Globally() 
         total_tuples | beam.io.WriteToText('number', file_name_suffix='.txt')
 
-def read_data_and_split(pipeline,input_file):
+def read_data_and_prepare(pipeline,input_file):
     lines = pipeline | beam.io.ReadFromText(input_file)
     words = lines | beam.FlatMap(lambda line: line.split())
-    return words
+    clean_words = words | beam.Map(healthy_text)
+    count_words = clean_words | beam.combiners.Count.PerElement()
+    return count_words
 
 if __name__ == '__main__':
     main()
